ed2bf9562006e0618eb4147fd481272565968270
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-10 15:41:30 +0800

    Do eg1 in section 4 of Part 1

---
+++ Part 1 - Fundamentals/4. Fundamental Data Types/eg1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    bool isNewUser = false;
+    cout << noboolalpha << isNewUser << endl;
+    cout << boolalpha << isNewUser << endl;
+    
+
+
+
+    system("PAUSE");
+    return 0;
+}
