47545b4ff638dd8fa18cf024f072246d7044038a
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-11 12:49:55 +0800

    Do eg3 in section 4 of Part 1

---
+++ Part 1 - Fundamentals/4. Fundamental Data Types/eg3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 10;
+    int m = 7;
+    double z =  static_cast<double>(a) / m ;
+    cout << z <<endl;
+
+    system("PAUSE");
+    return 0;
+}
