4780876aafc11659db73422dbbdfdd445c5caafb
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-11 14:15:55 +0800

    Do eg1 in section 5 of Part 1

---
+++ Part 1 - Fundamentals/5.Decision Making/eg1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+    int x = 10;
+    int y = 5;
+    bool result = x != y ;
+   
+    char first = 'a';
+    char second = 'A';
+    bool resultt = first == second;
+
+    cout << resultt << endl; 
+    cout << boolalpha<< result << endl ;
+
+
+
+    system("PAUSE");
+    return 0;
+}
