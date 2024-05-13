6901245db8d66dbbae41d3da29808e7c31e24845
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-13 13:00:30 +0800

    Do ex3 in section 5 of Part 1

---
+++ Part 1 - Fundamentals/5.Decision Making/ex3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int first;
+    int second;
+    cout<< "Enter two values :";
+    cin >> first>> second;
+
+    int result = (first > second) ? first : second ;
+
+    cout << "Answer : "<< result<< endl;
+
+
+
+
+
+
+    system("PAUSE");
+    return 0 ;
+}
