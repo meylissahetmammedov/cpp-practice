fef5ea2fcfae60ccaf3432ffece63ee2b332b389
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-14 16:08:06 +0800

    Do eg3 in section 5 of Part 1

--- Part 1 - Fundamentals/5.Decision Making/eg3.cpp
+++ Part 1 - Fundamentals/5.Decision Making/eg3.cpp
@@ -6,7 +6,7 @@ int main (){
     cout << "1- Create account" << endl
          << "2- Change password" << endl
          << "3- Quit " << endl
-         << " Select an option: "; 
+         << "Select an option: "; 
 
      short input;
     cin >> input;
