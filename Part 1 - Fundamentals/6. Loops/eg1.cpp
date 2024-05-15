77187245cf2cafea0bcf59188a22bdbc05c1fe9c
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-15 14:13:16 +0800

    Do eg1 in section 6 of Part 1

---
+++ Part 1 - Fundamentals/6. Loops/eg1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    for (int i =5 ; i >0 ; i--){
+        if(i % 2 !=0)
+        cout << i << endl;
+    }
+
+    for (int i = 0; i <5; i++)
+    {
+        cout <<i << endl;
+    }
+    
+
+
+
+
+    system ("PAUSE");
+    return 0 ;
+}
