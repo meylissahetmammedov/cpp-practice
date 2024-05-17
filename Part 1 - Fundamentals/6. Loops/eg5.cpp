c1aec9af65299fbec7de8d22ef26a84faa929c97
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-17 21:47:15 +0800

    Do eg5 in section 6 of Part 1

---
+++ Part 1 - Fundamentals/6. Loops/eg5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+   /* for (int i=1; i<=5; i++)
+    cout << i << endl;*/
+   /* int i = 5;
+    while (i<=5){
+        cout << i <<endl;
+        i++;
+    }*/
+    int number = 0;
+    while (number < 1 || number > 5){
+        cout << "Number : ";
+        cin  >> number;
+        /*if (number < 1 || number > 5)
+        cout << "Enter a number between 1 and 5!" << endl;*/
+    }
+    
+    
+
+
+
+    system("PAUSE");
+    return 0;
+}
