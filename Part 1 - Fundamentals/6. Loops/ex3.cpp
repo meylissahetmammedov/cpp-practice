7a536e9ab1f6c658c24ef28e45dcd712b1c5e908
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-17 21:47:32 +0800

    Do ex3 in section 6 of Part 1

---
+++ Part 1 - Fundamentals/6. Loops/ex3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+     int secret = 7 ;
+     int guess = 0;
+     while (guess  !=secret ){
+     cout << "Guess :";
+     cin >> guess;
+    }
+
+
+    system("PAUSE");
+    return 0;
+}
