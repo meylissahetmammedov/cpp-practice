0aa6f505c1c7baa3647133204f20f460481c3ef5
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-19 22:05:31 +0800

    Do ex4 in section 6 of Part 1

---
+++ Part 1 - Fundamentals/6. Loops/ex4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "Enter a Rows :";
+    int number1;
+    cin >> number1;
+    cout << "Enter second Rows :";
+    int number2;
+    cin >> number2;
+    
+    for(int x = 1; x <= number1; x++){
+        for (int y = 1; y <= x; y++){
+            cout << "*";  
+        }
+          cout << endl;
+       
+    }
+    
+    for(int m = 1; m <= number2; m++){
+        for (int n = number2; n >= m; n--){
+            cout << "*";  
+        }
+          cout << endl;
+    }
+    
+    system ("PAUSE");
+    return 0;
+}
