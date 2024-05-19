e0ff6196b9207759a49da169671d488c3e3b15ad
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-19 22:05:53 +0800

    Do eg6 in  section 6 of Part 1

---
+++ Part 1 - Fundamentals/6. Loops/eg6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int number ;
+    do {
+        cout << "Number :";
+        cin >> number;
+    } while (number < 1 || number > 5);
+    
+
+
+
+    system ("PAUSE");
+    return 0;
+}
