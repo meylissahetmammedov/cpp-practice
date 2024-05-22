00c4d5a11ed528944b4e2bc3aacc23c121b40f28
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-05-22 14:41:52 +0800

    Do eg4 in section 7 of Part 1__Passing_Arguments_by_Value_or_Reference

---
+++ Part 1 - Fundamentals/7. Functions/eg4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void increasePrice(double& price){
+    price *=1.2;
+}
+void greet(const string name){
+    cout << " Hello " << name << endl;
+}
+
+int main () {
+    double price = 100;
+    increasePrice(price);
+    cout << price << endl; 
+    
+    string name = "Meylis";
+    greet (name);
+
+
+    system("PAUSE");
+    return 0;
+}
