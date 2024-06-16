2d446220e9babd16b39b81edb4265c58537c8a25
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2024-06-16 11:35:06 +0800

    Do eg2 in section 2 of Part 2 Determining_the_Size_of_Arrays

---
+++ Part 2 - Intermediate/1.Arrays/eg2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    int numbers[]={10,20,30};
+    for(int i = 0; i < size(numbers); i++)
+        cout << numbers[i] << endl;
+        
+
+
+
+    system("Pause");
+    return 0;
+}
