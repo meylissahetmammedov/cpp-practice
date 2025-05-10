62091ea287a5afd23befdecf8a3de9c724415d63
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-11 01:19:48 +0800

    Do ex5 in section 4 Part 1

---
+++ Part 1 - Fundamentals/4. Fundamental Data Types/ex5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 3;
+    double z = x / y;
+    cout << z << endl;
+
+    system("PAUSE");
+    return 0;
+}
