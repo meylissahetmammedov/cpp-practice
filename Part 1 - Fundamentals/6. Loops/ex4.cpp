58738683608e16f4d73248369a7d22de1df5abd2
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-18 14:39:11 +0800

    Do ex6 (Nested Loops) in section 6 of Part 1

--- Part 1 - Fundamentals/6. Loops/ex4.cpp
+++ Part 1 - Fundamentals/6. Loops/ex4.cpp
@@ -2,29 +2,33 @@
 
 using namespace std;
 
-int main (){
+int main()
+{
     cout << "Enter a Rows :";
     int number1;
     cin >> number1;
     cout << "Enter second Rows :";
     int number2;
     cin >> number2;
-    
-    for(int x = 1; x <= number1; x++){
-        for (int y = 1; y <= x; y++){
-            cout << "*";  
+
+    for (int x = 1; x <= number1; x++)
+    {
+        for (int y = 1; y <= x; y++)
+        {
+            cout << "*";
         }
-          cout << endl;
-       
+        cout << endl;
     }
-    
-    for(int m = 1; m <= number2; m++){
-        for (int n = number2; n >= m; n--){
-            cout << "*";  
+
+    for (int m = 1; m <= number2; m++)
+    {
+        for (int n = number2; n >= m; n--)
+        {
+            cout << "*";
         }
-          cout << endl;
+        cout << endl;
     }
-    
-    system ("PAUSE");
+
+    system("PAUSE");
     return 0;
 }
