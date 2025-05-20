6a875747f54988cb49cccc523ba641e50ebf8c7c
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-20 22:33:52 +0800

    Do eg3 ( Overloading Functions) in section 7 of Part 1

--- Part 1 - Fundamentals/7. Functions/eg3.cpp
+++ Part 1 - Fundamentals/7. Functions/eg3.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
 using namespace std;
-void greet(string name){
+void greet(string name)
+{
     cout << "Hello " << endl;
 }
 // Signature = name + (number and type of parametrs)
-void greet (string title, string name){
-    cout << "Hello " << title << " " << name << endl;
+void greet(string title, string name, string age)
+{
+    cout << "Hello " << title << " " << name << endl
+         << " My age : " << age << endl;
 }
 
-int main (){
-    greet("Mr", "Meylis")  ;
+int main()
+{
+    greet("Mr", "Meylis", "23");
 
     system("PAUSE");
     return 0;
