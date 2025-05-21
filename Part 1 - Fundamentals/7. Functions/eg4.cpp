696f6527d3c5bd0d356b097708461717075c812e
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-21 21:56:59 +0800

    Do eg4 (Passing Argumnets by Value or Reference) in section 7 of Part 1

--- Part 1 - Fundamentals/7. Functions/eg4.cpp
+++ Part 1 - Fundamentals/7. Functions/eg4.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
 using namespace std;
-void increasePrice(double& price){
-    price *=1.2;
+void increasePrice(double &price)
+{
+    price *= 1.2;
 }
-void greet(const string name){
-    cout << " Hello " << name << endl;
+void greet(const string name)
+{
+    cout << "Hello " << name << endl;
 }
 
-int main () {
-    double price = 100;
+int main()
+{
+    double price = 50;
     increasePrice(price);
-    cout << price << endl; 
-    
-    string name = "Meylis";
-    greet (name);
+    cout << price << endl;
 
+    string name = "Meylis";
+    greet(name);
 
     system("PAUSE");
     return 0;
