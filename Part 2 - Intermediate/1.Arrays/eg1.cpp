5fb0697c9a0a72be03a4d5c5d28c0e014d3219c0
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-23 15:28:22 +0800

    Do eg1(Creating and initializing Arrays) in section 2 of Part 2

--- Part 2 - Intermediate/1.Arrays/eg1.cpp
+++ Part 2 - Intermediate/1.Arrays/eg1.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main (){
-    int numbers []={10,20};
-    cout << numbers<< endl;
-
+int main()
+{
+    int numbers[] = {10, 20, 30, 40};
+    cout << numbers << endl;
 
     system("Pause");
     return 0;
