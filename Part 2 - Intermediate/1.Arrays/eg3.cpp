5e7674cf3e91df97a18eb0fb61f0b2225f5c2c7e
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-05-24 21:12:54 +0800

    Do eg3 (Copying Arrays) in section 2 of Part 2

--- Part 2 - Intermediate/1.Arrays/eg3.cpp
+++ Part 2 - Intermediate/1.Arrays/eg3.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main (){
-    int first[]={10,20,30};
-    int second [size (first)];
+int main()
+{
+    int first[] = {10, 20, 30, 40, 44, 50};
+    int second[size(first)];
 
     for (int i = 0; i < size(first); i++)
-    second[i]= first[i];
+        second[i] = first[i];
 
-    for (int number: second )
-         cout << number << endl;
-
-
+    for (int number : second)
+        cout << number << endl;
 
     system("Pause");
     return 0;
