8a7668ade8063a9ed493580bbe90d76fbf097859
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-06-05 20:33:25 +0800

    Do eg4 (Pointer Arithmetic) in section 3 of Part 2

---
+++ Part 2 - Intermediate/2. Pointers/eg4_Pointer_Arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numbers[] = {10, 20, 33};
+    int *ptr = numbers;
+    cout << ptr[2] << endl;
+    // cout << *(ptr *2);
+    //  cour << numbers[1];
+    //  ptr++
+
+    system("PAUSE");
+    return 0;
+}
