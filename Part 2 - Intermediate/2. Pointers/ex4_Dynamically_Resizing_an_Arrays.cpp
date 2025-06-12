65474f039e9497723f29d4d4bc2f55f2405a939f
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-06-12 23:33:14 +0800

    Do ex4 (Dynamical Resizing an Arrays) in section 3 of Part 2

---
+++ Part 2 - Intermediate/2. Pointers/ex4_Dynamically_Resizing_an_Arrays.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int capacity = 5;
+    int *numbers = new int[capacity];
+    int entries = 0;
+
+    while (true)
+    {
+        cout << "Number: ";
+        cin >> numbers[entries];
+        if (cin.fail())
+            break;
+        entries++;
+        if (entries == capacity)
+        {
+            capacity *= 2;
+            // Create a temp array (twice the size)
+            int *temp = new int[10];
+            for (int i = 0; i < entries; i++)
+                temp[i] = numbers[i];
+            delete[] numbers;
+            numbers = temp;
+
+            // Copy all the elements
+            // Have "numbers" pointer point to the new array
+        }
+    }
+
+    for (int i = 0; i < entries; i++)
+        cout << numbers[i] << endl;
+
+    delete[] numbers;
+
+    system("PAUSE");
+    return 0;
+}
