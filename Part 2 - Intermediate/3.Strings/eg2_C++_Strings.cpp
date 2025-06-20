fa86490aef91cc9c89cacbeac5afea03b2fa4380
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-06-20 23:04:01 +0800

    Do eg2 (C++ Strings(concatenate/ birlesdirmek) in section 4 of Part 2

---
+++ Part 2 - Intermediate/3.Strings/eg2_C++_Strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Meylis";
+    name[0] = 'm';
+    name += " Sahetmammedov";
+    cout << name << endl;
+
+    system("PAUSE");
+    return 0;
+}
