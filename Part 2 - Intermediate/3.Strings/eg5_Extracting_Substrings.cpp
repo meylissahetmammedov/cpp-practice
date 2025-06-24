1aad2ef5b994e06f5c505afb11b44e83bc0e2808
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-06-24 21:51:01 +0800

    Do eg5 (Extracting Substrings) in section 4 of Part 2

---
+++ Part 2 - Intermediate/3.Strings/eg5_Extracting_Substrings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Meylis Sahetmammedov";
+    string copy = name.substr(7, 4);
+    cout << copy << endl;
+
+    system("PAUSE");
+    return 0;
+}
