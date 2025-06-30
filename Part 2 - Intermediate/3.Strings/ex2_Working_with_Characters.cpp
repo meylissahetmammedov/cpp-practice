6a6479f8afe4893c229be109202b3ac54c887260
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-06-30 23:00:23 +0800

    Do ex2 (Working with characters) ( ulanyja kod berilende ulanylya ) in section 4 of Part 2

--- Part 2 - Intermediate/3.Strings/ex2_Working_with_Characters.cpp
+++ Part 2 - Intermediate/3.Strings/ex2_Working_with_Characters.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+// bul c++ kody bize , ulanyja kod beremizde ulaylyan ,
 bool isValid(string customerNumber)
 {
-    if (customerNumber.length() != 6)
+    if (customerNumber.length() != 6) // barde kod 6 sifirdan ybarat bolmaly bolamsa / yalnys
         return false;
 
     for (int i = 0; i < 2; i++)
-        if (!isalpha(customerNumber[1]))
+        if (!isalpha(customerNumber[i])) // barde aytyanty 1-2 setirde harp bolmaly
             return false;
 
-    for (int i = 2; i < customerNumber.length(); i++)
+    for (int i = 2; i < customerNumber.length(); i++) // barde 3-nji setirden baslap 6 njy setire cenli san bolmaly
         if (!isdigit(customerNumber[i]))
             return false;
 
@@ -20,7 +20,13 @@ bool isValid(string customerNumber)
 
 int main()
 {
-    string customerNumber = "AB1234";
+    cout << "You  need write  6 characters first two need character alphabet and  3 until 6 numbers" << endl;
+    cout << " (If your code is right > 1 )" << endl;
+    cout << "         Else > 0         " << endl;
+    string customerNumber;
+    cout << "Enter code: " << endl;
+    cin >> customerNumber;
+
     cout << isValid(customerNumber) << endl;
 
     system("PAUSE");
