3885ba37c30a53347913ea155a5ebb24d46a9994
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-07-03 23:01:22 +0800

    Do eg8 (Escape Sequences)(asaky setir/ arasynda bosluk goyya)  in section 4 of Part 2

---
+++ Part 2 - Intermediate/3.Strings/eg8_Escape_Sequences.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str = " Hello \n World"; // Escapede  \n diyip  yazanda asaky setire gecya
+    cout << str << endl;
+    string str1 = "Hello \t World"; // \t diyip yazanda ini sozun arasyna  bosluk girya
+    cout << str1 << endl;
+
+    system("PAUSE");
+    return 0;
+}
