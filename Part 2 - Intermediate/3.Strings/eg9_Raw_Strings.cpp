53b825c01cfb2b99fa00373c455897ea153bf564
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-07-04 19:15:44 +0800

    Do eg9 (Raw Strings) in section 4 of Part 2

---
+++ Part 2 - Intermediate/3.Strings/eg9_Raw_Strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str = R"(c:\folderA\folderB\file.txt")";
+    cout << str << endl;
+
+    system("PAUSE");
+    return 0;
+}
