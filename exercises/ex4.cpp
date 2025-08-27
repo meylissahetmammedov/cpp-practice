0c466bfa136b39a66666d5d5715a17f07c132db9
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-08-27 16:52:14 +0800

    b number how many times have in  (a) number

---
+++ exercises/ex4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Birinji sany giriz: ";
+    cin >> a;
+    cout<< "Ikinji sany giriz: ";
+    cin >> b;
+
+
+    int c = a/b;//b san a sanda nace sapar bar
+
+
+    cout<< "Netije: "<<c << endl;
+
+    return 0;
+}
