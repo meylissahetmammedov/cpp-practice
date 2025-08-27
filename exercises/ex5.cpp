f76508d0e2a9270bf3f0f0f88fa615efd9b37725
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-08-27 16:57:38 +0800

    The remainder of the numbers a and b

---
+++ exercises/ex5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace  std;
+
+int main() {
+    int a,b;
+    cout << "Birinji sany giriz: ";
+    cin >> a;
+    cout<< "Ikinji sany giriz: ";
+    cin >> b;
+
+
+    int c = a%b; //ulanylamdyk bolek
+
+
+    cout<< "Netije: "<<c << endl;
+}
