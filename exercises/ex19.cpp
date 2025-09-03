444943231d0779cfaa84143ade9d3d8c252373ce
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-03 23:15:36 +0800

    Ex19,Convert the given seconds to minutes

---
+++ exercises/ex19.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "Sekuntdan minda gecirmli san girizin: ";
+    cin >> a;
+
+    int b = a /60;
+    //Beeilen sekundy , minut wagtyna  gecirmneli
+    cout << "Jogap: "<< b <<" minut"<<endl;
+
+    int pause;
+    cin >> pause;
+    return 0;
+}
