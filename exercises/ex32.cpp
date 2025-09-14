3943344e6a9f37134e6363b8c849711c0a6940cf
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-14 17:27:58 +0800

    Ex32,When a number between 1 and 12 is entered, the number indicates the season

---
+++ exercises/ex32.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    short  san ;
+    cout<< "1-12 araklyk ayy nomerni girizin: ";
+    cin >> san;
+
+    switch (san) {
+        case 1:
+            cout<< "Gys pasly";
+            break;
+        case 2:
+            cout<< "Gys pasly";
+            break;
+        case 3:
+            cout<< "Yaz pasly";
+            break;
+        case 4:
+            cout<< "Yaz pasly";
+            break;
+        case 5:
+            cout<< "Yaz pasly";
+            break;
+        case 6:
+            cout<< "Tomus pasly";
+            break;
+        case 7:
+            cout<< "Tomus pasly";
+            break;
+        case 8:
+            cout<< "Tomus pasly";
+            break;
+        case 9:
+            cout<< "Guyz pasly";
+            break;
+        case 10:
+            cout<< "Guyz pasly";
+            break;
+        case 11:
+            cout<< "Guyz pasly";
+            break;
+        case 12:
+            cout<< "Gys pasly";
+            break;
+
+    }
+
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
