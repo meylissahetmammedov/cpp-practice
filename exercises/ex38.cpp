1172d9444090dd9d7038eae6eadf11218a236513
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-20 18:35:34 +0800

    Ex38

---
+++ exercises/ex38.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a ;
+    cout<< " Gun senani giriz: ";
+    cin >>  a;
+
+
+    int b;
+    cout << "Ayy girizmeli: ";
+    cin >> b;
+
+    int c ;
+    cout << "Yyl girizmeli: ";
+    cin >> c;
+
+    cout << "Senin beren senanin bir gun on: " << a+1<<"." << b << "."<< c << endl;
+
+
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
