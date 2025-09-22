949e3cd84b389e981f2b413691359026789b3431
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-22 20:34:22 +0800

    Ex40, If the given number is positive, add 1, otherwise subtract 2

---
+++ exercises/ex40.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a ;
+    cout<< " San giriz: ";
+    cin >>  a;
+
+
+
+    if ( a > 0  ) {
+        cout << "Jogap:" << a+1 ;
+    }else{
+        cout << "Jogap:" << a -2  << endl ;
+    }
+    // eger berilen san polazitel bolsa onda 1 gosamaly , bolamsa 2 san ayrmaly
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
