312b82b7c79696c7a1b4174c9db303aa6b2a66af
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-26 15:59:43 +0800

    Ex44,Enter two numbers and find which is larger

---
+++ exercises/ex44.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a ,b  ;
+    cout<< " Birinjy san giriz: ";
+    cin >>  a;
+    cout<< " Ikinji san giriz: ";
+    cin >>  b;
+
+
+    if ( a > b) {
+        cout << "Senin beren sanyn ulysy : "<< a << endl;
+    }else {
+        cout << "Senin beren sanyn ulysy : "<< b << endl;
+    }
+
+    //Iki sany san girizmeli olaryn haysynyn ulydygyny tapmaly
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
