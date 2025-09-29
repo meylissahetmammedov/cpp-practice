7f8a1dd81700ad83bc60a34d71d35fde27a13722
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-09-29 22:39:05 +0800

    Ex47

---
+++ exercises/ex47.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a ,b  ;
+    cout<< " A san giriz: ";
+    cin >>  a;
+    cout<< " B san giriz: ";
+    cin >>  b;
+
+
+    int kici = min(a, b);
+    int  uly = max(a, b);
+
+    a = kici;
+    b = uly;
+
+    cout<< "A kici san: " << a << endl;
+    cout<< "B uly san: " << b << endl;
+
+    //A,B san girizende her gezek a ve b sanlaryny uytgedip a san kici san edip cykarmaly
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
