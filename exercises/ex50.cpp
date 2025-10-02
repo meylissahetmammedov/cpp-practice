1bd2ed8e16bd8b7409cb0b4362b3d45ed60f5c7e
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-02 21:10:40 +0800

    Ex50

---
+++ exercises/ex50.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a ,b ,c ;
+    cout<< " 1-njy san giriz: ";
+    cin >>  a;
+    cout<< " 2-nji  san giriz: ";
+    cin >>  b;
+    cout<< " 3 - nji  san giriz: ";
+    cin >>  c;
+
+    int kici = a;
+
+    if(kici > b) {
+        kici = b;
+    }if (kici > c) {
+        kici = c;
+    }
+
+    cout << "In kici san: "<< kici;
+
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
