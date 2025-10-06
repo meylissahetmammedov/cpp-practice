58b58d10acb0f23a4b2eb457d3be119a6008e632
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-06 16:52:44 +0800

    Ex51.2

---
+++ exercises/ex51.2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    cout << " 1-njy san giriz: ";
+    cin >> a;
+    cout << " 2-nji  san giriz: ";
+    cin >> b;
+    cout<< " 3 - nji  san giriz: ";
+    cin >> c;
+
+    int min = a;
+    if (b < min) {
+        min = b;
+    }
+    if (c < min) {
+        min = c;
+    }
+
+    int max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+
+
+    if (a > min && a < max) {
+        cout << "Ortaky san: " << a << endl;
+    }
+    if (b > min && b < max) {
+        cout << "Ortaky san: " << b << endl;
+    }
+    if (c > min && c < max) {
+        cout << "Ortaky san: " << c << endl;
+    }
+
+
+
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
