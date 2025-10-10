381c69e2e4fa8f41ef73f27951f156f8cf03de52
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-10 20:45:15 +0800

    Ex55

---
+++ exercises/ex55.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double a, b, c;
+    cout << " 1-njy san giriz: ";
+    cin >> a;
+    cout << " 2-nji  san giriz: ";
+    cin >> b;
+    cout<< " 3 - nji  san giriz: ";
+    cin >> c;
+
+    if ( a <= b && b <= c) {
+        cout << "Jogap: " << a *2 << ", " << b *2 <<", " << c* 2 <<  endl;
+    }if (a >= b && b >= c) {
+        cout << " Jogap: " << a *2 << ", " << b *2 <<", " << c* 2 <<  endl;
+    }else {
+        cout << " Jogap: " << a * -1 << ", "<< b * -1 << ", "<< c * -1 <<  endl;
+    }
+
+    
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
