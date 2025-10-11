74a56fb4fad25aa318cec392dca18f3855d8d653
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-11 16:54:38 +0800

    Ex56, Finds a number that is not the same as the numbers entered

---
+++ exercises/ex56.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    double a, b, c;
+    cout << " 1-njy san giriz: ";
+    cin >> a;
+    cout << " 2-nji  san giriz: ";
+    cin >> b;
+    cout<< " 3 - nji  san giriz: ";
+    cin >> c;
+    // Girizen sanlaryndan menzes dalini capa cykarya
+    if ( a == b) {
+        cout << "Jogap: " << c <<  endl;
+    }else if (a == c) {
+        cout << " Jogap: " << b <<  endl;
+    }else if (b == c){
+        cout << " Jogap: " << a <<  endl;
+    }else {
+        cout << "Hic biri menzes dal!!";
+    }
+    int pause;
+    cin >> pause;
+
+    return 0;
+}
