8f9c359717be24f6cb80b36d9b4b5a0969d4a583
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-12 19:34:40 +0800

    Ex57

---
+++ exercises/ex57.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    double a, b, c, d;
+    cout << " 1-njy san giriz: ";
+    cin >> a;
+    cout << " 2-nji  san giriz: ";
+    cin >> b;
+    cout<< " 3 - nji  san giriz: ";
+    cin >> c;
+    cout << " 4 - nji  san giriz: ";
+    cin >> d;
+
+    // Girizen sanlaryndan menzes dalini capa cykarya
+    int pause;
+    if ( a == b &&  a == c) {
+        cout << "Jogap: " << d <<  endl;
+    }else if (a == b && a == d ) {
+        cout << " Jogap: " << c <<  endl;
+    }else if (a  == c && a == d ) {
+        cout << " Jogap: " << b <<  endl;
+    }else if (b  == c && b == d ) {
+        cout << " Jogap: " << a <<  endl;
+    }else {
+        cout << "Hic biri menzes dal!!";
+    }
+    cin >> pause;
+
+    return 0;
+}
