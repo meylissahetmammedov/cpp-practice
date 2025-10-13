ecf8f9a71a018c356e4644effa090eb44a29f537
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-13 20:09:28 +0800

    Ex58

---
+++ exercises/ex58.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "A, B, C nokatlaryny giriz!! "<< endl;
+    int a, b, c;
+    cout << "A nokat: "  ;
+    cin >> a;
+    cout << "B nokat: " ;
+    cin >>  b;
+    cout << "C nokat: "  ;
+    cin >> c;
+
+
+    int b_uzaklyk = b - a;
+    int c_uzaklyk = c - a;
+
+    cout << "B nokatdan A nokada uzaklyk: "<< b_uzaklyk << endl;
+    cout << "C nokatdan A nokada uzaklyk: "<< c_uzaklyk << endl<< endl;
+
+    if (b_uzaklyk < c_uzaklyk) {
+        cout << "B - nokady A nokadyna has yakyn"<<endl;
+        cout<< "Uzaklyk: " << b_uzaklyk << endl;
+
+    }else if (c_uzaklyk < b_uzaklyk  ) {
+        cout << "C - nokady A nokadyna has yakyn"<<endl;
+        cout<< "Uzaklyk: " << c_uzaklyk << endl;
+    }else {
+        cout << "B we C nokatlar A-ya den uzaklykda" << endl;
+        cout << "Uzaklyk: " << b_uzaklyk;
+    }
+
+
+    return 0;
+}
