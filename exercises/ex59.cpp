a984333a8d226eea14cba0af2da4463f926c3fe0
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-10-14 16:56:40 +0800

    Ex59

---
+++ exercises/ex59.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "A, B nokatlaryny giriz!! "<< endl;
+    int OX, OY;
+    cout << "A nokat: "  ;
+    cin >> OX;
+    cout << "B nokat: " ;
+    cin >>  OY;
+
+    if (OX == 0 && OY == 0) {
+        cout <<"Jogap: " << 0;
+    } else if (OX == 0) {
+        cout <<"Jogap: " << 1;
+    }else if (OY == 0) {
+        cout <<"Jogap: " << 2;
+    }else {
+        cout <<"Jogap: " << 3;
+    }
+
+    return 0;
+}
