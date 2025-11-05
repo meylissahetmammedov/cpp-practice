7edb988e58c2f05bf3f3f604c25681bd18eb7ec2
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-11-05 18:41:40 +0800

    Ex76

---
+++ exercises/ex76.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout << "A sanyny giriz:";
+    cin >> a;
+
+    if (a < 0 ) {
+        cout << "Yalnys!! A > 0 bolmaly!" << endl;
+
+        return 1;
+    }
+
+    int jem = 0;
+
+    cout << "Sanlar :  "<< endl;
+
+
+    for (int i = 0; i <= a; i++) {
+        jem += (a + i) * (a + i);
+        if (i == 0) {
+            cout << a << "^2 " << "+";
+
+        }else if (i == a) {
+            cout << "(2 * " << a << ")^2" << " = ";
+        } else {
+            cout << " ( " << a << " + " << i << " )^2 + " ;
+        }
+    }
+
+    cout << jem << endl;
+
+    return 0;
+}
