336f4f4694f72b99bc1ea6159b0241782115708a
Meylis Sahetmammedov <meylissahetmammedow@gmail.com>
2025-11-14 20:55:04 +0800

    update

--- exercises/ex70.cpp
+++ exercises/ex70.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "Suyjinin bahasyny giriz:";
     cin >> suyjininBahasy;
 
+
     if (suyjininBahasy <= 0) {
         cout << " (0 dan uly san girizmeli!!)" << endl;
         return 1;
